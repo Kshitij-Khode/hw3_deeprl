Fixed Gt targets calculation for reinforce

src/reinforce.py
@@ -15,6 +15,8 @@ class Reinforce(object):
         # TODO: Define any training operations and optimizers here, initialize
         #       your variables, or alternately compile your model here.
 
+        print('Reinforce __init__: lr:%s' % lr)
+
         self.sess = tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True)))
 
         keras.backend.tensorflow_backend.set_session(self.sess)
@@ -52,18 +54,24 @@ class Reinforce(object):
             if ep % saveInt == 0: self.save_model_weights(ep)
 
             states, actions, rewards = self.generate_episode(env)
-            trew                     = [r/1e-2 for r in rewards]
+            trew                     = [r*1e-2 for r in rewards]
 
-            print('ep:%s, lRew:%s' % (ep, rewards[-1]))
-
-            Gt = [sum(gamma**d * rew for d, rew in enumerate(trew[k:]))
-                                     for k in xrange(len(trew))]
+            Gt = [np.sum([pow(gamma,k-t)*trew[k] for k in xrange(t,len(trew))])
+                                                 for t in xrange(len(trew))]
             Gt = np.tile(np.matrix(Gt), (4,1)).transpose()
+
+            print('ep:%s, len:%s, Gt[0]:%s' % (ep, len(trew), Gt[0]))
 
             _, loss = self.sess.run([self.trainOp, self.loss], feed_dict={
                 self.inpState: states, self.execAct: actions, self.trgRew: Gt
             })
 
+        self.save_model_weights(numEps)
+
+
+    def test(self, env, numEps):
+        for ep in range(numEps):
+            states, actions, rewards = self.generate_episode(env)
 
     def generate_episode(self, env):
         # Generates an episode by executing the current policy in the given env.
@@ -78,11 +86,16 @@ class Reinforce(object):
         state   = env.reset()
 
         for _ in itertools.count():
-            # env.render()
+            env.render()
 
             actionProbs = self.model.predict(np.expand_dims(state,0))[0]
-            action      = np.random.choice(np.arange(len(actionProbs)), p=actionProbs)
+
+            action = np.random.choice(np.arange(len(actionProbs)), p=actionProbs)
             nstate, rew, term, _ = env.step(action)
+
+            # print('actionInt:%s, actionProbs:%s, rew:%s' % (np.arange(len(actionProbs)),
+            #     actionProbs,
+            #     rew))
 
             if term: break
 
@@ -98,6 +111,8 @@ class Reinforce(object):
         self.model.save_weights('./store/%s_weights.ckpt' % prefix, overwrite=True)
         print('saved ./model/%s_weights.ckpt' % prefix)
 
+    def load_model_weights(self, weight_file):
+        self.model.load_weights(weight_file)
 
 def parse_arguments():
     # Command-line flags are defined here.
@@ -105,10 +120,11 @@ def parse_arguments():
     parser.add_argument('--model-config-path', dest='model_config_path',
                         type=str, default='LunarLander-v2-config.json',
                         help="Path to the model config file.")
+    parser.add_argument('--weight-path', dest='weight_path', type=str)
     parser.add_argument('--num-episodes', dest='num_episodes', type=int,
                         default=50000, help="Number of episodes to train on.")
     parser.add_argument('--lr', dest='lr', type=float,
-                        default=1e-3, help="The learning rate.")
+                        default=5e-4, help="The learning rate.")
 
     return parser.parse_args()
 
@@ -118,7 +134,8 @@ def main(args):
     args              = parse_arguments()
     model_config_path = args.model_config_path
     num_episodes      = args.num_episodes
-    lr                = args.lr
+    lr                = args.lr*1e+2
+    weight_path       = args.weight_path
 
     # Create the environment.
     env = gym.make('LunarLander-v2')
@@ -128,7 +145,10 @@ def main(args):
 
     # TODO: Train the model using REINFORCE and plot the learning curve.
     reInfModel = Reinforce(model, lr)
-    reInfModel.train(env, num_episodes)
+    # reInfModel.train(env, num_episodes)
+
+    reInfModel.load_model_weights(weight_path)
+    reInfModel.test(env, num_episodes)
 
 
 if __name__ == '__main__':